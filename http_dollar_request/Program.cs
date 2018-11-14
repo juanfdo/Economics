0534799a929dc3f826d12d39a4d6bfaeb23e755e
juanfdo <juan.fdo@outlook.com>
2018-11-13 21:24:31 -0500

    petición http para obtener el precio del dolar

---
+++ http_dollar_request/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace http_dollar_request
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			MainClass.recuest();
+            
+        }
+		public static string recuest(){
+			HttpWebRequest peticion = (HttpWebRequest)WebRequest.Create("http://www.elcolombiano.com");         
+			HttpWebResponse respuesta = (HttpWebResponse)peticion.GetResponse();
+			string html = new StreamReader(respuesta.GetResponseStream()).ReadToEnd();
+
+			string aBuscar = "<div class=\"indicador\"> <h5>DOLAR</h5> <span> $";
+
+			int i = html.IndexOf(aBuscar, StringComparison.InvariantCultureIgnoreCase);
+			i += aBuscar.Length;
+			int f = html.IndexOf("\r", i, StringComparison.InvariantCultureIgnoreCase);
+
+			string str = html.Substring(i,f-i);
+            
+            Console.WriteLine(str);
+			return (str);
+		}
+    }
+}
